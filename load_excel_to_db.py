7a1b9cb677a794a2b4bfc323da7171ce18e7ac9e
anushh-git <ms.anusri2003@gmail.com>
2026-06-25 10:10:55 +0000

    Database commit of sqlite pushing

---
+++ load_excel_to_db.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import sqlite3
+
+# Path to your Excel file
+EXCEL_FILE = r"C:/Users/anushka-sr/Desktop/demo/inventory.xlsx"
+
+# Load Excel
+df = pd.read_excel(EXCEL_FILE)
+
+# Clean column names (VERY IMPORTANT)
+df.columns = df.columns.str.strip()
+
+# Connect to SQLite database
+conn = sqlite3.connect("inventory.db")
+
+# Push into database
+df.to_sql(
+    "MedicineInventory",
+    conn,
+    if_exists="replace",   # overwrites old empty table
+    index=False
+)
+
+conn.close()
+
+print("Excel data successfully loaded into database")
